c97c7707f6a0642f29623d18cecec13cdc55a64f
Matsuuu <huhta.matias@gmail.com>
2021-09-08 19:40:49 +0300

    Card component

---
+++ src/components/card/card.styles.ts
@@ -0,0 +1,46 @@
+import { css } from 'lit';
+import { baseStyles } from '@simplr-wc/components-core';
+
+export const cardStyles = [
+    baseStyles,
+    css`
+        :host {
+            min-width: 260px;
+            width: fit-content;
+            max-width: 330px;
+            border-radius: 4px;
+            box-shadow: 0px 3px 3px -2px rgba(0, 0, 0, 0.2), 0px 2px 2px 0px rgba(0, 0, 0, 0.14),
+                0px 1px 8px 0px rgba(0, 0, 0, 0.12);
+            display: flex;
+            flex-direction: column;
+            background: #fff;
+        }
+        .content {
+            padding: 1rem;
+        }
+        h2,
+        p {
+            margin: 0;
+        }
+        h2 {
+            font-size: 1.25rem;
+            margin-bottom: 0.25rem;
+        }
+        p {
+            font-size: 0.875rem;
+            opacity: 0.6;
+        }
+        slot[name='media']::slotted(*) {
+            border-radius: 4px 4px 0 0;
+        }
+
+        slot::slotted(p) {
+            font-size: 0.875rem;
+            opacity: 0.6;
+        }
+        slot[name='actions']::slotted(div) {
+            padding: 1rem;
+            border-top: 1px solid #d6d1e0;
+        }
+    `,
+];
---
+++ stories/card.stories.ts
@@ -0,0 +1,40 @@
+import { html, TemplateResult } from 'lit';
+import '@simplr-wc/card';
+import { Story } from './story-types.js';
+
+export default {
+    title: 'Card',
+    component: 'simplr-card',
+    argTypes: {},
+};
+
+export interface ArgTypes {
+    label: string,
+    subtitle: string,
+    slot: TemplateResult,
+    imageslot: TemplateResult
+}
+
+const Card: Story<ArgTypes> = ({ label, subtitle, slot, imageslot }: ArgTypes) => html`
+    <simplr-card
+        .label=${label}
+        .subtitle=${subtitle}
+            >
+            ${imageslot}
+
+            ${slot}</simplr-card> `;
+
+export const Regular = Card.bind({});
+Regular.args = {
+    label: "Card label",
+    subtitle: "Card subtitle",
+    slot: html`<p>This is my body</p>`
+}
+
+export const WithImage = Card.bind({});
+WithImage.args = {
+    label: "Card label",
+    subtitle: "Card subtitle",
+    slot: html`<p>This is my body</p>`,
+    imageslot: html`<img src="https://picsum.photos/300/200" slot="media" />`
+}
