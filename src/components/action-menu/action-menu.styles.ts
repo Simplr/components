75a1cbc6c14b17334ebe3c707ff89ed6514b8c42
Matsuuu <huhta.matias@gmail.com>
2021-09-27 18:27:55 +0300

    Action menu

---
+++ src/components/action-menu/action-menu.styles.ts
@@ -0,0 +1,36 @@
+import { css } from 'lit';
+import { baseStyles } from '@simplr-wc/components-core';
+
+export const actionMenuStyles = [
+    baseStyles,
+    css`
+        :host {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            width: fit-content;
+            height: fit-content;
+            --size: 18px;
+            --height: var(--size);
+            --width: var(--size);
+            cursor: pointer;
+            transition: 100ms ease-in-out;
+        }
+
+        :host([vertical]) {
+            transform: rotate(90deg);
+            transform-origin: center;
+            margin-bottom: 0.25rem;
+        }
+
+        :host(:hover) {
+            fill: var(--primary-color);
+            color: var(--primary-color);
+        }
+
+        ::slotted(*) {
+            height: var(--height);
+            width: var(--width);
+        }
+    `,
+];
---
+++ src/components/action-menu/action-menu.ts
@@ -0,0 +1,56 @@
+import { html, LitElement } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
+import { actionMenuStyles } from './action-menu.styles';
+import '@simplr-wc/components-core/loading';
+
+@customElement('simplr-action-menu')
+export class SimplrActionMenu extends LitElement {
+    @property({ type: Boolean, reflect: true })
+    open: boolean = false;
+
+    @property({ type: Boolean, reflect: true })
+    vertical: boolean = false;
+
+    @property({ type: String, reflect: true })
+    for: string | undefined;
+
+    firstUpdated() {
+        this.addEventListener('click', this.toggle);
+    }
+
+    toggle() {
+        this.open = !this.open;
+        const target = document.querySelector(`#${this.for}`) as any;
+        if (this.open) {
+            target.open();
+        } else {
+            target.close();
+        }
+    }
+
+    render() {
+        return html`<slot>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                xmlns:xlink="http://www.w3.org/1999/xlink"
+                version="1.1"
+                id="Capa_1"
+                width="18px"
+                heigth="18px"
+                x="0px"
+                y="0px"
+                viewBox="0 0 32.055 32.055"
+                style="enable-background:new 0 0 32.055 32.055;"
+                xml:space="preserve"
+            >
+                <path
+                    d="M3.968,12.061C1.775,12.061,0,13.835,0,16.027c0,2.192,1.773,3.967,3.968,3.967c2.189,0,3.966-1.772,3.966-3.967   C7.934,13.835,6.157,12.061,3.968,12.061z M16.233,12.061c-2.188,0-3.968,1.773-3.968,3.965c0,2.192,1.778,3.967,3.968,3.967   s3.97-1.772,3.97-3.967C20.201,13.835,18.423,12.061,16.233,12.061z M28.09,12.061c-2.192,0-3.969,1.774-3.969,3.967   c0,2.19,1.774,3.965,3.969,3.965c2.188,0,3.965-1.772,3.965-3.965S30.278,12.061,28.09,12.061z"
+                />
+            </svg>
+        </slot>`;
+    }
+
+    static get styles() {
+        return actionMenuStyles;
+    }
+}
