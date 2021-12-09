e31f634469669012054b76e6de907a4842006712
Matsuuu <huhta.matias@gmail.com>
2021-12-09 18:47:15 +0200

    Export base styles

--- src/core/index.ts
+++ src/core/index.ts
@@ -3,3 +3,5 @@ export * from './theme/ripple.styles.js';
 export * from './util/chronology.js';
 export * from './util/keyboard.js';
 export * from './util/find.js';
+
+export { baseStyles as SIMPLR_THEME_STYLES } from './theme/base.styles';
